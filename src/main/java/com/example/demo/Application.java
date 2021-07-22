6db1156dd345f769aad7ae524d35c0eaf4071888
Arilson Almada <arilson.almada@gmail.com>
2021-07-22 14:32:04 -0700

    first commit

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,13 +1,27 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
+    @Autowired
+    private StudentService studentService;
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner (StudentService studentService){
+        return args -> {
+            Student maria = new Student("Maria", "Jones", "maria.jones@gmail.com", 31);
+            studentService.saveStudent(maria);
+        };
     }
 
 }
